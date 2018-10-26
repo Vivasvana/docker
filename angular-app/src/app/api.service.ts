43897d5acbf88cdeabccf4528f88f5ca0addd4ff
Yevhen Shevchenko <yevhen.shevchenko@codeminers.com.ua>
2018-10-26 14:17:17 +0300

    Fix inteface

--- angular-app/src/app/api.service.ts
+++ angular-app/src/app/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Book} from "./books/book";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
+import {BookDetail} from "./book-detail/book-detail";
 
 @Injectable()
 export class ApiService {
@@ -15,7 +16,7 @@ export class ApiService {
     return this.http.get<Book[]>(this.apiUrl + '/books');
   }
 
-  public getBook(id: number): Observable<Book>{
-    return this.http.get<Book>(this.apiUrl + '/books/' + id);
+  public getBook(id: number): Observable<BookDetail>{
+    return this.http.get<BookDetail>(this.apiUrl + '/books/' + id);
   }
 }
